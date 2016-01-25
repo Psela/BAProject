ce4f4a033418f1d2f06f0629ed5aff267349681a
teejayt <tolanijayt4ril@yahoo.com>
2016-01-25 16:45:30 +0000

    Worked on the coursecatalogue view

--- BA Project/BA Project/Controllers/CourseCatalogueController.cs
+++ BA Project/BA Project/Controllers/CourseCatalogueController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace BA_Project.Controllers
 {
     public class CourseCatalogueController : Controller
     {
+        //get the data from the database
+        
+        //public System.Data.Entity<Dbset> getData() {
+
+        //    using (var context = new BAProjectEntities())
+        //    {
+        //       context.users.
+        //        for (int i = 0; i < length; i++)
+        //        {
+                    
+        //        }
+        //    }
+        //}
+
         // GET: CourseRegistration
         public ActionResult Index()
         {
             return View();
         }
+
+
     }
 }

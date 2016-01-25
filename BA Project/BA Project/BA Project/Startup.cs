9af6d38c8b7a690a9eadc3ddd28428992365bb08
psela <gael.bouyer@fdmgroup.com>
2016-01-25 09:28:35 +0000

    INitial

---
+++ BA Project/BA Project/BA Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BA_Project.Startup))]
+namespace BA_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

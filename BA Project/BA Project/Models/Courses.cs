e7e0f58b4d51cd53216407a3e6674ae2b92cfb9f
Francelp <francel_piedra@hotmail.com>
2016-01-26 17:16:40 +0000

    IndividualCourses

---
+++ BA Project/BA Project/Models/Courses.cs
@@ -0,0 +1,27 @@
+﻿using DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BA_Project.Models
+{
+    public class Courses
+    {
+        public List<cours> listOfCourses;
+
+        public List<cours> GetListOfCourses()
+        {
+            listOfCourses = new List<cours>();
+            using (var context = new BAProjectEntities())
+            {
+                foreach (cours course in context.courses)
+                {
+                    listOfCourses.Add(course);
+                }
+            }
+
+            return listOfCourses;
+        }
+    }
+}

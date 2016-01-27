88140984f64584de7dbe3baede524eb6d23858cc
psela <gael.bouyer@fdmgroup.com>
2016-01-27 17:26:17 +0000

    updated database

--- BA Project/BA Project/Controllers/CourseCatalogueController.cs
+++ BA Project/BA Project/Controllers/CourseCatalogueController.cs
@@ -10,46 +10,81 @@ using System.Windows.Forms;
 
 namespace BA_Project.Controllers
 {
-    public class CourseCatalogueController : Controller
+  public class CourseCatalogueController : Controller
+  {
+    Courses courses = new Courses();
+    Professor professor = new Professor();
+    public List<cours> listOfCourses;
+
+    public ActionResult Index()
     {
-        Courses courses = new Courses();
-        Professor professor = new Professor();
-        public List<cours> listOfCourses;
-
-        public ActionResult Index()
-        {
-            return View(listOfCourses);
-        }
-
-        public ActionResult IndividualCourse(string id)
-        {
-            cours courseToDisplay = listOfCourses.FirstOrDefault(x => x.course_id.ToString() == id);
-            return View(courseToDisplay);
-        }
-
-        public ActionResult LecturerName(string id) //Finds the lecturer using the course id
-        {
-            user prof = professor.GetProfessorById(id);
-
-            return PartialView(prof);
-        }
-
-        public CourseCatalogueController()
-        {
-            listOfCourses = courses.GetListOfCourses();
-        }
-
-        public ActionResult courseInfo()
-        {
-            return View();
-        }
-        public ActionResult courseHistory()
-        {
-            return View();
-        }
-        public ActionResult confirmationPage()
-        {
-            return View();
-        }
+      return View(listOfCourses);
     }
+
+    public ActionResult IndividualCourse(string id)
+    {
+      cours courseToDisplay = listOfCourses.FirstOrDefault(x => x.course_id.ToString() == id);
+      return View(courseToDisplay);
+    }
+
+    //Finds the lecturer using the course id
+    public ActionResult LecturerName(string id)
+    {
+      user prof = professor.GetProfessorById(id);
+
+      return PartialView(prof);
+    }
+
+    public CourseCatalogueController()
+    {
+      listOfCourses = courses.GetListOfCourses();
+    }
+
+    public ActionResult courseInfo()
+    {
+      return View();
+    }
+
+    public ActionResult courseHistory()
+    {
+      return View();
+    }
+
+    public ActionResult confirmationPage()
+    {
+      return View();
+    }
+
+    public void NewCourse(string name, string outline, string start, string end)
+    {
+      using (var context = new BAProjectEntities())
+      {
+        string username = Request.Cookies["user"].Value;
+        user user = context.users.FirstOrDefault(x => x.username.Equals(username));
+        int type = user.type_of_user;
+        bool approved = false;
+        int lecturer = 0;
+        if (type == 3)
+        {
+          approved = true;
+        }
+        else if (type == 1)
+        {
+          lecturer = user.users_id;
+        }
+        cours course = new cours()
+        {
+          name = name,
+          outline = outline,
+          //start_date = (DateTime)20021022,
+          //finish_date = end,
+          available = true,
+          approved = approved,
+          lecturer = lecturer
+        };
+        context.courses.Add(course);
+        context.SaveChanges();
+      }
+    }
+  }
 }

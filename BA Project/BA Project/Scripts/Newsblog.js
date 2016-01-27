bf47cc24817f352e3b4a8cd90f3a083bfe1863f4
teejayt <tolanijayt4ril@yahoo.com>
2016-01-27 19:16:25 +0000

    Made a lot of changes to the homepage. The homepage is completed

--- BA Project/BA Project/Scripts/Newsblog.js
+++ BA Project/BA Project/Scripts/Newsblog.js
@@ -1,27 +1,9 @@
 ﻿
 $(document).ready(function () {
-    $("#general").click(function () {
 
-        document.getElementById("changeNews").innerHTML = "general";
-          
-    });
-    $("#admission").click(function () {
 
-        document.getElementById("changeNews").innerHTML = "admission";
 
-    });
-
-    $("#staff").click(function () {
-
-        document.getElementById("changeNews").innerHTML = "staff";
-
-    });
-
-    $("#excursion").click(function () {
-
-        document.getElementById("changeNews").innerHTML = "excursion";
-
-    });
+    $('#tabs').tab();
    
 }
 );

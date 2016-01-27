1a80e50c05bc5ed0e260544bdf7262f77248f379
psela <gael.bouyer@fdmgroup.com>
2016-01-27 15:04:28 +0000

    Report Card category

--- BA Project/BA Project/Controllers/ReportCardController.cs
+++ BA Project/BA Project/Controllers/ReportCardController.cs
@@ -11,6 +11,24 @@ namespace BA_Project.Controllers
 {
   public class ReportCardController : Controller
   {
+    public ActionResult Index()
+    {      
+      user user = new user();
+      string cookie = HttpContext.Request.Cookies["user"].Value;
+      using (var context = new BAProjectEntities())
+      {
+        user = context.users.FirstOrDefault(x => x.username.Equals(cookie));
+      }
+      if (user.type_of_user == 2)
+      {
+        return StudentView(user.users_id);
+      }
+      else
+      {
+        return LecturerView("1");
+      }
+    }
+
     //view for the lecturer
     public ActionResult LecturerView(string id)
     {
@@ -60,9 +78,19 @@ namespace BA_Project.Controllers
     }
 
     //view for student report card
-    public ActionResult StudentView()
+    public ActionResult StudentView(int id)
     {
-      return View();
+      Dictionary<string, string> grades = new Dictionary<string, string>();
+      using (var context = new BAProjectEntities())
+      {
+        List<grades_database> gradeDB = context.grades_database.Where<grades_database>(x => x.student_id.Equals(id)).ToList();
+        foreach (var item in gradeDB)
+        {
+          grades.Add(item.cours.name,item.grade);
+        }
+      }
+
+      return View(grades);
     }
 
   }
